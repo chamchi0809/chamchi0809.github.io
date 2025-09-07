49eb099cea41543409dacf0b306ad4a23e4525d6
chamchi0809 <seoshi999@gmail.com>
2025-09-07 18:02:21 +0900

    feat: Videogame 모델 추가 및 MonitorBG 컴포넌트 수정

--- src/components/MonitorBG.tsx
+++ src/components/MonitorBG.tsx
@@ -1,7 +1,7 @@
-import {Canvas, useFrame, useThree} from "@react-three/fiber";
-import {Radio} from "./Radio.tsx";
-import {CameraShake, ContactShadows, Html} from "@react-three/drei";
-import {type ReactNode, useState} from "react";
+import {Canvas, useThree} from "@react-three/fiber";
+import Videogame from "./Videogame.tsx";
+import {CameraShake, ContactShadows, Float, Html} from "@react-three/drei";
+import {useState} from "react";
 import * as THREE from "three";
 import {DoubleSide} from "three";
 
@@ -11,13 +11,15 @@ export default function MonitorBG() {
     return <div style={{position: "absolute", inset: 0}}>
         <Canvas shadows dpr={[1, 2]} style={{width: '100%', height: '100%'}} camera={[0, 0, CAM_DISTANCE, {fov: 50}] as any}>
             <directionalLight castShadow intensity={4} shadow-mapSize={2048} position={[0, 0, 0]} rotation={[-1, 0, 0]}/>
-            <ambientLight intensity={1.5}/>
-            <Radio position={[0, -1, 0]} scale={2}/>
-            <Html transform castShadow receiveShadow scale={.2} position={[2, -.5, 0]} rotation={[.2,0,0]}
-                  material={<meshStandardMaterial side={DoubleSide} opacity={.1}/>}>
-                <Card/>
-            </Html>
-            <ContactShadows position={[0, -1.2, 0]} opacity={1} scale={10} blur={3} far={10} resolution={256} color="#000000"/>
+            <ambientLight intensity={1.2}/>
+            <Float rotationIntensity={2}>
+                <Videogame position={[-.8, -1, 0]} rotation={[.2, 0, 0]} scale={.2}/>
+                <Html occlude={"raycast"} transform castShadow receiveShadow scale={.2} position={[.8, 0, 0]} rotation={[.2, 0, 0]}
+                      material={<meshStandardMaterial side={DoubleSide} opacity={.1}/>}>
+                    <Card/>
+                </Html>
+            </Float>
+            <ContactShadows position={[0, -1.2, 0]} opacity={1} scale={10} blur={1} far={10} resolution={256} color="#000000"/>
             <Rig/>
             <fog attach="fog" args={['lightpink', 0, 90]}/>
         </Canvas>
@@ -37,8 +39,8 @@ const Card = () => {
         flexDirection: "column",
         alignItems: "center",
         borderRadius: 8,
-        background:"white",
-        overflow:"hidden",
+        background: "white",
+        overflow: "hidden",
         width: 300,
         position: "relative",
     }}>
@@ -75,7 +77,7 @@ const QuickLink = (
     {
         link,
         icon,
-    }:{
+    }: {
         link: string;
         icon: string;
     }) => {
@@ -89,7 +91,7 @@ const Tag = (
     {
         icon,
         text,
-    }:{
+    }: {
         icon: string;
         text: string;
     }) => {
